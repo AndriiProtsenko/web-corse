5a53555d00d16902a5fb2b02cfe7018d874c485f
AndriiProtsenko <protsenko.andrii@lll.kpi.ua>
2023-05-14 16:50:40 +0300

    Update lab

--- laba7/laba7.js
+++ laba7/laba7.js
@@ -1,84 +1,59 @@
-//1. Написати функцію з одним аргументом, яка перевіряє чи є аргумент числом.
-// Якщо так, то повертає чи є це цисло парним чи не парним.Якщо аргумент не число - повертає пусту строку.
+//1.
 function Task1(value) {
-    if (typeof (value) !== 'number') {
-        return "";
+    if (typeof value !== 'number') return "";
+    return value % 2 === 0 ? "Число парне" : "Число не парне";
     }
-    if (value % 2 === 0) {
-        return "Число парне";
-    } else {
-        return "Число не парне";
-    }
-}
 
-//2. Написати функцію, яка починає перебирати числа від 1 до нескінченності і перевіряє чи є число
-// простим(просте число - те яке ділиться лише на 1 чи само на себе).Якщо просто число знайдене -
-// воно додається до масиву.Після пʼятого знайденого простого числа функція повинна повернути суму
-// всіх пʼяти простих чисел в масиві.
-function Task2() {
+    console.log("T1.1: Передача string: " + Task1("ab"));
+    console.log("T1.2: Передача boolean: " + Task1(true));
+    console.log("T1.3: Передача number = 3: " + Task1(3));
+    console.log("T1.4: Передача number = 2: " + Task1(2));
+
+    //2.
+    function Task2() {
     let number = 1;
     let simpleNumbers = [];
-
-    for (let i = 0; i < 5; number++) {
+    
+    for (let i = 0; i < 5;) {
         if (ValueIsSimple(number)) {
-            simpleNumbers[i] = number;
+            simpleNumbers.push(number);
             i++;
         }
+        number++;
     }
     return SumOfArrayNumbers(simpleNumbers);
-}
-function ValueIsSimple(value) {
-    if (value <= 1)
-        return false;
+    }
+    
+    function ValueIsSimple(value) {
+    if (value <= 1) return false;
+    
 
-    for (let i = 2; i < value; i++) {
+    for (let i = 2; i <= Math.sqrt(value); i++) {
         if (value % i === 0) {
             return false;
         }
     }
-
     return true;
-}
-function SumOfArrayNumbers(array) {
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
+    }
+    
+    function SumOfArrayNumbers(array) {
+    return array.reduce((acc, curr) => acc + curr, 0);
     }
 
-    return sum;
-}
-//3. Написати функцію з одним аргументом n, що є числом.Функція повинна повернути суму наступного ряду
-// з n чисел 1 + 11 + 111 + 1111 + ...., де кожний елемент - число з постійно зростаючою кількістю одиниць.
-function Task3(n) {
+console.log("T2: Сума перших п'яти простих чисел: " + Task2());
+    //3.
+    function Task3(n) {
     let array = [];
-    let row = "";
+    let row = "1";
+    
 
     for (let i = 0; i < n; i++) {
-        if (row === "") {
-            row = "1";
-        } else {
-            row += "1";
-        }
-
         array[i] = Number(row);
+        row += "1";
+    }
+    return SumOfArrayNumbers(array);
     }
 
-    return SumOfArrayNumbers(array);
-}
-console.log("Task1. Виводимо результат, коли передаємо string: " + Task1("sdsds"));
-console.log("Task1. Виводимо результат, коли передаємо boolean: " + Task1(true));
-console.log("Task1. Виводимо результат, коли передаємо number = 1: " + Task1(1));
-console.log("Task1. Виводимо результат, коли передаємо number = 4: " + Task1(4));
-
-console.log("Task2. Сума перших 5ти простих чисел: " + Task2());
-
-console.log("Task3. n = 77: " + Task3(3));
-console.log("Task3. n = 4: " + Task3(4));
-console.log("Task3. n = 11: " + Task3(11));
-
-// ----------------------------
-
-// код для того, щоб консоль автоматично не закривалася
-var readline = require('readline');
-var rl = readline.createInterface(process.stdin, process.stdout);
-// --
+console.log("T3.1: n = 3: " + Task3(3));
+console.log("T3.2: n = 4: " + Task3(4));
+console.log("T3.3: n = 6: " + Task3(6));
